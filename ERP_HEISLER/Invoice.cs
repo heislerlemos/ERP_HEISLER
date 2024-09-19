5c8c21e0479b6f846304a1b56143ad5b70b0d916
heisler lemos <heisler.stlano@gmail.com>
2024-09-19 12:38:24 +0100

    adicionando imagem na biblioteca graphics

--- ERP_HEISLER/Invoice.cs
+++ ERP_HEISLER/Invoice.cs
@@ -31,19 +31,21 @@ namespace ERP_HEISLER
 		
 		}
 
-		Bitmap bmp;
-
 
 		private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
 		{
+			Image imageFile = Image.FromFile("C:\\Users\\heisler.lemos\\source\\repos\\heislerlemos\\ERP_HEISLER\\ERP_HEISLER\\Resources\\logo_invoice.jpeg");
+			Image image = (Bitmap)imageFile.Clone();
 
-			e.Graphics.DrawString("Fatura", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(370, 10));
-			e.Graphics.DrawString("-----------------------------", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(300, 20));
-			e.Graphics.DrawString("Heisler ERP, Lda", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(300, 40));
-			e.Graphics.DrawString("Rua Santa Catarina", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(300, 60));
-			e.Graphics.DrawString("Total", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(300, 100));
-			e.Graphics.DrawString("-----", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(300, 120));
-			e.Graphics.DrawString(faturalabel.Text + " MIL " + "KZ", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(300, 140));
+			e.Graphics.DrawImage(imageFile, 370,10,55,35);
+
+			e.Graphics.DrawString("Fatura Cliente", new Font("Arial", 15, FontStyle.Regular), Brushes.Black, new Point(330, 50));
+			e.Graphics.DrawString("-----------------------------", new Font("Arial", 15, FontStyle.Regular), Brushes.Black, new Point(300, 70));
+			e.Graphics.DrawString("Heisler ERP, Lda", new Font("Arial", 15, FontStyle.Regular), Brushes.Black, new Point(300, 90));
+			e.Graphics.DrawString("Rua Santa Catarina", new Font("Arial", 15, FontStyle.Regular), Brushes.Black, new Point(300, 110));
+			e.Graphics.DrawString("Total", new Font("Arial", 15, FontStyle.Regular), Brushes.Black, new Point(300, 140));
+			e.Graphics.DrawString("-----------------------------", new Font("Arial", 15, FontStyle.Regular), Brushes.Black, new Point(300, 160));
+			e.Graphics.DrawString(faturalabel.Text + " MIL " + "KZ", new Font("Arial", 15, FontStyle.Regular), Brushes.Black, new Point(300, 180));
 		}
 	}
 }
